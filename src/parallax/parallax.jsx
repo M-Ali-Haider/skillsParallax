39817725969ffae58edc65bc71ca38f0f1c51cae
M-Ali-Haider <mah01134212100@gmail.com>
2024-02-11 02:38:26 +0500

    hmmmm

--- src/parallax/parallax.jsx
+++ src/parallax/parallax.jsx
@@ -6,20 +6,6 @@ import { useEffect, useRef } from 'react'
 import Lenis from '@studio-freight/lenis'
 import useDimension from './useDimension'
 import { skills } from './skills'
-const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-]
 
 export default function Parallax() {
 
